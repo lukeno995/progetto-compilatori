fb3681900e959746abe092f410ecfacffe00b05f
christian <christian.nocerino@securebit.it>
2022-03-17 19:21:36 +0100

    added function c

---
+++ test/myfun.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+float somma(float a, float b) {
+float result;
+printf("STRING_CONST\n"); 
+result = a + b;
+return result;
+
+}
+float moltiplica(float a, float b) {
+float result;
+printf("STRING_CONST\n"); 
+if (a == REAL_CONST || b == REAL_CONST) {
+result = REAL_CONST;
+}
+else {
+result = a * b;
+
+}
+return result;
+
+}
+float dividi(float a, float b) {
+float result;
+printf("STRING_CONST\n"); 
+if (b == REAL_CONST) {
+result = REAL_CONST;
+}
+else {
+result = a / b;
+
+}
+return result;
+
+}
+float sottrai(float a, float b) {
+float result;
+printf("STRING_CONST\n"); 
+if (a < b) {
+result = REAL_CONST;
+}
+else {
+result = a - b;
+
+}
+return result;
+
+}
+void stampa(char* messaggio) {
+i;
+while (i <= INTEGER_CONST) {
+incremento;
+printf("STRING_CONST\n"); 
+i = i + incremento;
+    }
+printf("messaggio\n"); 
+
+}int main() {
+float a = REAL_CONST;
+float b = REAL_CONST;
+op;
+ans;
+int operator = INTEGER_CONST;
+while (ans == STRING_CONST) {
+ans = STRING_CONST;
+printf("op\n"); 
+printf("STRING_CONST\n"); 
+printf("operator\n"); 
+if (operator == INTEGER_CONST) {
+result = (a, b);
+;
+}
+if (operator == INTEGER_CONST) {
+result = (a, b);
+;
+}
+if (operator == INTEGER_CONST) {
+result = (a, b);
+;
+}
+if (operator == INTEGER_CONST) {
+result = (a, b);
+;
+}
+(STRING_CONSTresult);
+printf("STRING_CONST\n"); 
+    }
+printf("STRING_CONST\n"); 
+printf("STRING_CONST\n"); 
+
+    return 0;
+}
